2bb00e4ba94d5f33b8a425c0de443c3b816f78d6
mu001999 <mu001999@outlook.com>
2019-05-09 12:10:56 +0800

    init

---
+++ sub/AES/aes.cpp
@@ -0,0 +1,42 @@
+#include <aes.hpp>
+
+using namespace std;
+
+// AES 128
+vector<uint32_t>
+AES::encrypt::AES128(const vector<uint32_t> &plaintext, const array<uint32_t, 4> &key)
+{
+    
+}
+
+vector<uint32_t>
+AES::decrypt::AES128(const vector<uint32_t> &plaintext, const array<uint32_t, 4> &key)
+{
+    
+}
+
+// AES 192
+vector<uint32_t>
+AES::encrypt::AES192(const vector<uint32_t> &plaintext, const array<uint32_t, 6> &key)
+{
+    
+}
+
+vector<uint32_t>
+AES::decrypt::AES192(const vector<uint32_t> &plaintext, const array<uint32_t, 6> &key)
+{
+    
+}
+
+// AES 256
+vector<uint32_t>
+AES::encrypt::AES256(const vector<uint32_t> &plaintext, const array<uint32_t, 8> &key)
+{
+    
+}
+
+vector<uint32_t>
+AES::decrypt::AES256(const vector<uint32_t> &plaintext, const array<uint32_t, 8> &key)
+{
+    
+}
