bc1ba8920743b8d7540d91c4c6a3b15485c5c7f5
mu001999 <mu001999@outlook.com>
2019-05-22 14:56:11 +0800

    update ase.hpp

--- sub/AES/aes.hpp
+++ sub/AES/aes.hpp
@@ -4,27 +4,27 @@
 
 namespace AES
 {
-    namespace encrypt
-    {
-        std::vector<std::uint32_t>
-        AES128(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 4> &key);
+namespace encrypt
+{
+std::vector<std::uint32_t>
+AES128(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 4> &key);
 
-        std::vector<std::uint32_t>
-        AES192(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 6> &key);
-        
-        std::vector<std::uint32_t>
-        AES256(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 8> &key);
-    }
+std::vector<std::uint32_t>
+AES192(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 6> &key);
 
-    namespace decrypt
-    {
-        std::vector<std::uint32_t>
-        AES128(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 4> &key);
-        
-        std::vector<std::uint32_t>
-        AES192(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 6> &key);
-        
-        std::vector<std::uint32_t>
-        AES256(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 8> &key);
-    }
+std::vector<std::uint32_t>
+AES256(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 8> &key);
+}
+
+namespace decrypt
+{
+std::vector<std::uint32_t>
+AES128(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 4> &key);
+
+std::vector<std::uint32_t>
+AES192(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 6> &key);
+
+std::vector<std::uint32_t>
+AES256(const std::vector<std::uint32_t> &plaintext, const std::array<std::uint32_t, 8> &key);
+}
 }
