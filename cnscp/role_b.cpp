3f953c396405736e936aca4ef8a960c887d3c449
mu001999 <mu001999@outlook.com>
2019-05-31 16:53:13 +0800

    init role b

---
+++ cnscp/role_b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Bank Server Init..." << endl;
+    cout << "Bank Server Start..." << endl;
+
+    return 0;
+}
